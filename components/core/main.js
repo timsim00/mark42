9a0fba3f261c14877af3836306990f72ca35e841
Drew Simmons <simmons.drew@gmail.com>
2019-05-23 13:26:05 -0500

    cleanup

--- components/core/main.js
+++ components/core/main.js
@@ -17,26 +17,6 @@
       this.render(this.store.state)
     },
 
-    // render(state) {
-    //   // render main view bc of state change:
-    //   let {type, app, status} = state.app.view.next || {}
-    //   console.log('core-main render', type, ':', app)
-    //   if (!app) { return }
-    //
-    //   const showingElement = document.querySelector(`${type}-${app}`)
-    //   console.log('core-main render showingElement', !!showingElement)
-    //   if (showingElement) { return } // we already have this on the screen
-    //
-    //   console.log('core-main rendering')
-    //
-    //   this.removeAttribute('hidden')
-    //
-    //   document.querySelector('#CoreMain_template').innerHTML = `<${type}-${app} hidden></${type}-${app}>`
-    //   const ele = document.querySelector(`${type}-${app}`)
-    //   ele.render(this.store.state)
-    //   ele.removeAttribute('hidden')
-    //   this.store.dispatch('app:SET_VIEW', {type, app, status: 'added'})
-    // }
     render(state) {
       // render main view bc of state change:
       let {elementName, status} = state.app.view.next || {}
